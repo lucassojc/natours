6c9a1643e4253db82716d84aff16c395d0ee8d6b
Luka <luka.jovanovic.1995@gmail.com>
2020-08-15 19:40:54 +0200

    Errors Outside Express: Unhandled Rejections

--- server.js
+++ server.js
@@ -19,16 +19,22 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log(
-      'DB connection established!'
-    );
+    console.log('DB connection established!');
   });
 
 // START SERVER
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
   console.log(
-    `App running on port ${port}`
+    'UNHANDLED REJECTION! Shutting down...'
   );
+  server.close(() => {
+    process.exit(1); // shut down app
+  });
 });
