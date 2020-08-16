2c09a617b1ad54e01f10ba28d7fb736fa20f0925
Luka <luka.jovanovic.1995@gmail.com>
2020-08-16 19:15:13 +0200

    Setting Security HTTP Headers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,12 +11,17 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (
   process.env.NODE_ENV === 'development' //log only if we are in development mode
 ) {
   app.use(morgan('dev')); //HTTP request logger middleware
 }
 
+// Limit requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -24,9 +30,13 @@ const limiter = rateLimit({
 }); // allow 100 request form the same IP per hour
 app.use('/api', limiter); // apply to all routs that start with /api
 
-app.use(express.json()); //middleware for converting body to json
+// Body parser, reading data from the body into req.body
+app.use(express.json({ limit: '10kb' })); // if we have body larger than 10kb it will not be accepted
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
