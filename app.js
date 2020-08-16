c52c4dd209ac579cdc7eff38b6b2350fcafd1af5
Luka <luka.jovanovic.1995@gmail.com>
2020-08-16 19:36:35 +0200

    Data Sanitization

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -32,6 +34,12 @@ app.use('/api', limiter); // apply to all routs that start with /api
 
 // Body parser, reading data from the body into req.body
 app.use(express.json({ limit: '10kb' })); // if we have body larger than 10kb it will not be accepted
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize()); // remove all $ and . from req.body and req.params
+
+// Data sanitization against XSS attacks
+app.use(xss()); // remove all user input with html code with attached js
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
