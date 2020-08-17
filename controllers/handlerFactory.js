fd7eb179ead674507727f8bfc7e9b5b05a706990
Luka <luka.jovanovic.1995@gmail.com>
2020-08-17 20:06:42 +0200

    Factory Functions: Update and Create

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -18,3 +18,40 @@ exports.deleteOne = (Model) =>
       data: null,
     });
   });
+
+exports.updateOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true, // return newly created document
+        runValidators: true, // run validators when updating
+      }
+    );
+
+    if (!doc) {
+      return next(
+        new AppError('No document found with that ID', 404)
+      );
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+exports.createOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -20,20 +20,15 @@ exports.getAllReviews = catchAsync(
   }
 );
 
-exports.createReview = catchAsync(
-  async (req, res, next) => {
-    // Allow nested routes
-    if (!req.body.tour) req.body.tour = req.params.tourId;
-    if (!req.body.user) req.body.user = req.user.id;
-    const newReview = await Review.create(req.body);
+exports.setTourUserIds = (req, res, next) => {
+  // Allow nested routes
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
+  next();
+};
 
-    res.status(201).json({
-      status: 'success',
-      data: {
-        review: newReview,
-      },
-    });
-  }
-);
+exports.createReview = factory.createOne(Review);
+
+exports.updateReview = factory.updateOne(Review);
 
 exports.deleteReview = factory.deleteOne(Review);
